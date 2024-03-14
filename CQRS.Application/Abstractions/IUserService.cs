e61cf66021cec08035bd9cfe827691394c5c52b4
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-03-14 19:13:47 +0500

    At Least 20% left.

---
+++ CQRS.Application/Abstractions/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Domain.Entities.DTOs;
+using CQRS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Application.Abstractions
+{
+    public interface IUserService
+    {
+        public Task<User> CreateAsync(User entity);
+        public Task<User> GetByAnyAsync(Expression<Func<User, bool>> expression);
+        public Task<IEnumerable<User>> GetAllAsync();
+        public Task<bool> DeleteAsync(Expression<Func<User, bool>> expression);
+        public Task<User> UpdateAsync(int id, UserDTO user);
+    }
+}
---
+++ CQRSUnitTest/UserServices.cs
@@ -0,0 +1,11 @@
+namespace CQRSUnitTest
+{
+    public class UserServices
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
